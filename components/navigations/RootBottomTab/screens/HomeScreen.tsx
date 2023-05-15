6d9433fa0ab110253c1af3edef695e6c1800bd26
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-05-15 15:45:19 +0700

    Adding Scroll View

--- components/navigations/RootBottomTab/screens/HomeScreen.tsx
+++ components/navigations/RootBottomTab/screens/HomeScreen.tsx
@@ -1,22 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen: React.FC = () => {
     const insets = useSafeAreaInsets();
 
+    function renderRows() {
+        const data: string[] = ['Hello World', 'Singa', "Europe", "Truck", "Simulator", 'Test123', 'Duar', 'Apa aja', 'Train 2023', 'Exposure', 'Zomboid']
+
+        const rowDataComponent = data.map((value, index) =>
+            <View key={index} style={styles.row}>
+                <Text style={styles.text}>{value}</Text>
+            </View>
+        );
+
+        return rowDataComponent;
+    }
+
     return (
-        <View style={{
-            ...styles.container,
-            paddingTop: insets.top,
-            paddingBottom: insets.bottom,
-            paddingStart: insets.left,
-            paddingEnd: insets.right
-        }}>
-            <Text>Home App!</Text>
-            <StatusBar style="auto" />
-        </View>
+        <ScrollView style={{ flex: 1 }}>
+            <View style={{
+                ...styles.container,
+                paddingTop: insets.top,
+                paddingBottom: insets.bottom,
+                paddingStart: insets.left,
+                paddingEnd: insets.right
+            }}>
+                {renderRows()}
+                <StatusBar style="auto" />
+            </View>
+        </ScrollView>
     );
 }
 
@@ -24,7 +38,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
         // justifyContent: 'center',
     },
+    row: {
+        flex: 1,
+        borderColor: 'grey',
+        borderWidth: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
+    text: {
+        fontSize: 20
+    }
 });
