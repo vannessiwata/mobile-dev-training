753d44d515f12b4d84a93a81549dd4f766ea69f7
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-05-15 14:50:54 +0700

    add logo

--- components/navigations/RootBottomTab/RootBottomTab.tsx
+++ components/navigations/RootBottomTab/RootBottomTab.tsx
@@ -1,13 +1,28 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { HomeScreen } from "./screens/HomeScreen";
 
+import FontAwesome from '@expo/vector-icons/FontAwesome5';
+
 const Tab = createBottomTabNavigator();
 
 export const RootBottomTab: React.FC = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen} />
-            <Tab.Screen name="Students" component={HomeScreen} />
+            <Tab.Screen name="Home" component={HomeScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <FontAwesome name="home" color={color} size={size}></FontAwesome>
+                )
+            }} />
+            <Tab.Screen name="Students" component={HomeScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <FontAwesome name="graduation-cap" color={color} size={size}></FontAwesome>
+                )
+            }} />
+            <Tab.Screen name="Trainers" component={HomeScreen} options={{
+                tabBarIcon: ({ color, size }) => (
+                    <FontAwesome name="chalkboard-teacher" color={color} size={size}></FontAwesome>
+                )
+            }} />
         </Tab.Navigator>
     );
 }
