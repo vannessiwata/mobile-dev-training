57e6fc18b1b5e13fa53240b7db1318b2f9713686
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-05-15 15:55:21 +0700

    adding flatlist

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ export default function App() {
     <SafeAreaProvider>
       <NavigationContainer>
         <RootNavigationStack />
+        <StatusBar style='auto' />
       </NavigationContainer>
     </SafeAreaProvider>
   );
--- components/navigations/RootBottomTab/screens/HomeScreen.tsx
+++ components/navigations/RootBottomTab/screens/HomeScreen.tsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const HomeScreen: React.FC = () => {
     const insets = useSafeAreaInsets();
+    const insetsStyling = StyleSheet.create({
+        insets: {
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            paddingStart: insets.left,
+            paddingEnd: insets.right
+        }
+    })
+    const data: string[] = ['Hello World', 'Singa', "Europe", "Truck", "Simulator", 'Test123', 'Duar', 'Apa aja', 'Train 2023', 'Exposure', 'Zomboid']
 
     function renderRows() {
-        const data: string[] = ['Hello World', 'Singa', "Europe", "Truck", "Simulator", 'Test123', 'Duar', 'Apa aja', 'Train 2023', 'Exposure', 'Zomboid']
-
         const rowDataComponent = data.map((value, index) =>
             <View key={index} style={styles.row}>
                 <Text style={styles.text}>{value}</Text>
@@ -19,18 +26,26 @@ export const HomeScreen: React.FC = () => {
     }
 
     return (
-        <ScrollView style={{ flex: 1 }}>
-            <View style={{
-                ...styles.container,
-                paddingTop: insets.top,
-                paddingBottom: insets.bottom,
-                paddingStart: insets.left,
-                paddingEnd: insets.right
-            }}>
-                {renderRows()}
-                <StatusBar style="auto" />
-            </View>
-        </ScrollView>
+        // <ScrollView style={{ flex: 1 }}>
+        //     <View style={{
+        //         ...styles.container,
+        //         paddingTop: insets.top,
+        //         paddingBottom: insets.bottom,
+        //         paddingStart: insets.left,
+        //         paddingEnd: insets.right
+        //     }}>
+        //         {renderRows()}
+        //         <StatusBar style="auto" />
+        //     </View>
+        // </ScrollView>
+
+        <FlatList data={data}
+            renderItem={(data) =>
+                <View key={data.index} style={styles.row}>
+                    <Text style={styles.text}>{data.item}</Text>
+                </View>
+            }
+            style={insetsStyling.insets} />
     );
 }
 
